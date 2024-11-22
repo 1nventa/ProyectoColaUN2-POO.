3141d08647827e0665de16eaa3e3d107ac27fee5
1nventa <chernandezl1@unicartagena.edu.co>
2024-11-22 16:21:25 -0500

    Add files via upload

---
+++ taller9/ejercicio1/Main.java
@@ -0,0 +1,7 @@
+package taller9.ejercicio1;
+public class Main {
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado("Juan", 30, "Recursos Humanos");
+        empleado.mostrarDetalles();
+    }
+}
---
+++ taller9/ejericicio3/Planta.java
@@ -0,0 +1,9 @@
+package taller9.ejericicio3;
+
+public class Planta {
+    private String tipo;
+
+    public Planta(String tipo) {
+        this.tipo = tipo;
+    }
+}
