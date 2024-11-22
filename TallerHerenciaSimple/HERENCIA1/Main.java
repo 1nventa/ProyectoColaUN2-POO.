46680f87f960ee61cecd47d4f2d20ad76bd61de0
1nventa <chernandezl1@unicartagena.edu.co>
2024-11-22 16:11:40 -0500

    Delete TallerHerenciaSimple directory

--- TallerHerenciaSimple/HERENCIA1/Main.java
+++
@@ -1,8 +0,0 @@
-package TallerHerenciaSimple.HERENCIA1;
-
-public class Main {
-    public static void main(String[] args) {
-        Coche miCoche = new Coche("Toyota", 180, 4);
-        miCoche.mostrarInformacion();
-    }
-}
--- TallerHerenciaSimple/HERENCIA2/Main.java
+++
@@ -1,8 +0,0 @@
-package TallerHerenciaSimple.HERENCIA2;
-
-public class Main {
-    public static void main(String[] args) {
-        Estudiante estudiante = new Estudiante("Juan Acevedo", 18, "30240052005205205");
-        estudiante.mostrarDetalles();
-    }
-}
